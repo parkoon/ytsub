bdf6b01e6ac4f7e10491ff86a5c6f85ed0f9329f
parkoon <devparkoon@gmail.com>
2025-12-07 09:22:02 +0900

    feat: add new icons and web manifest for improved app branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,6 +52,25 @@ export const metadata: Metadata = {
   creator: 'YTSub',
   publisher: 'YTSub',
   metadataBase: new URL('https://ytsub.org'),
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/android-chrome-192x192.png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/android-chrome-512x512.png',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
   alternates: {
     canonical: '/',
   },
